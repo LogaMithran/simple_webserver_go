4d808b556da3150b34ae659e318e5085d76191ed
logamithran <logamithran.k@caratlane.com>
2024-10-29 19:53:43 +0530

    work: initialized work dir

--- basics/main.go
+++ basics/main.go
@@ -3,7 +3,7 @@ package main
 import "basics/ErrorBasics"
 
 func main() {
-	//ErrorBasics.CalculateError()
-	//ErrorBasics.DeferBasics()
+	ErrorBasics.CalculateError()
+	ErrorBasics.DeferBasics()
 	ErrorBasics.PanicBasics()
 }
